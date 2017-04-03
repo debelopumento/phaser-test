cdab493dde8650764be32beadb23cc376f041c81
debelopumento <imdiye@gmail.com>
2017-04-03 13:20:47 -0700

    initial commit

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,34 +1,50 @@
 /* globals __DEV__ */
-import Phaser from 'phaser'
-import Mushroom from '../sprites/Mushroom'
+import Phaser from 'phaser';
+//import Mushroom from '../sprites/Mushroom';
 
 export default class extends Phaser.State {
-  init () {}
-  preload () {}
+  init() {}
+  preload() {}
 
-  create () {
-    const bannerText = 'Phaser + ES6 + Webpack'
-    let banner = this.add.text(this.world.centerX, this.game.height - 80, bannerText)
-    banner.font = 'Bangers'
-    banner.padding.set(10, 16)
-    banner.fontSize = 40
-    banner.fill = '#77BFA3'
-    banner.smoothed = false
-    banner.anchor.setTo(0.5)
+  create() {
+    //create world
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    this.mushroom = new Mushroom({
-      game: this,
-      x: this.world.centerX,
-      y: this.world.centerY,
-      asset: 'mushroom'
-    })
+    this.game.add.sprite(0, 0, 'star');
 
-    this.game.add.existing(this.mushroom)
+    const platforms = this.game.add.group();
+    platforms.enableBody = true;
+    const ground = platforms.create(0, this.game.world.height - 64, 'platform');
+    ground.scale.setTo(2, 2);
+    ground.body.imovable = true;
+    let ledge = platforms.create(100, 200, 'platform');
+    ledge.scale.setTo(0.4, 0.7);
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(350, 100, 'platform');
+    ledge.scale.setTo(0.5, 0.9);
+    ledge.body.immovable = true;
+
+    //create player
+    const player = this.game.add.sprite(
+      32,
+      this.game.world.height - 150,
+      'dude'
+    );
+    this.game.physics.arcade.enable(player);
+    player.body.bounce.y = 0.2;
+    player.body.gravity.y = 300;
+    player.body.collideWorldBounds = true;
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+
+    const cursors = this.game.input.keyboard.createCursorKeys();
   }
 
-  render () {
-    if (__DEV__) {
-      this.game.debug.spriteInfo(this.mushroom, 32, 32)
-    }
+  update() {
+    //console.log(this.game.physics.arcade.collide);
+    //this.game.physics.arcade.collide(this.player, this.platforms);
   }
+
+  render() {}
 }
--- src/states/Splash.js
+++ src/states/Splash.js
@@ -1,22 +1,31 @@
-import Phaser from 'phaser'
-import { centerGameObjects } from '../utils'
+import Phaser from 'phaser';
+import { centerGameObjects } from '../utils';
 
 export default class extends Phaser.State {
-  init () {}
+  init() {}
 
-  preload () {
-    this.loaderBg = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBg')
-    this.loaderBar = this.add.sprite(this.game.world.centerX, this.game.world.centerY, 'loaderBar')
-    centerGameObjects([this.loaderBg, this.loaderBar])
+  preload() {
+    this.loaderBg = this.add.sprite(
+      this.game.world.centerX,
+      this.game.world.centerY,
+      'loaderBg'
+    );
+    this.loaderBar = this.add.sprite(
+      this.game.world.centerX,
+      this.game.world.centerY,
+      'loaderBar'
+    );
+    centerGameObjects([this.loaderBg, this.loaderBar]);
 
-    this.load.setPreloadSprite(this.loaderBar)
-    //
-    // load your assets
-    //
-    this.load.image('mushroom', 'assets/images/mushroom2.png')
+    this.load.setPreloadSprite(this.loaderBar);
+    this.load.image('sky', 'assets/images/sky.png');
+    this.load.image('star', 'assets/images/star.png');
+    this.load.spritesheet('dude', 'assets/images/dude.png', 32, 48);
+    this.load.image('mushroom', 'assets/images/mushroom2.png');
+    this.load.image('platform', 'assets/images/platform.png');
   }
 
-  create () {
-    this.state.start('Game')
+  create() {
+    this.state.start('Game');
   }
 }
