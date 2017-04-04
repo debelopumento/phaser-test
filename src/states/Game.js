a73e47f30258aec55a665b52de973adee815c59f
debelopumento <imdiye@gmail.com>
2017-04-03 20:33:30 -0700

    collision is working

--- src/states/Game.js
+++ src/states/Game.js
@@ -1,6 +1,6 @@
 /* globals __DEV__ */
 import Phaser from 'phaser';
-//import Mushroom from '../sprites/Mushroom';
+import Mushroom from '../sprites/Mushroom';
 
 export default class extends Phaser.State {
   init() {}
@@ -8,13 +8,13 @@ export default class extends Phaser.State {
 
   create() {
     //create world
-    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.physics.startSystem(Phaser.Physics.ARCADE);
 
-    this.game.add.sprite(0, 0, 'star');
+    this.add.sprite(0, 0, 'star');
 
-    const platforms = this.game.add.group();
+    let platforms = this.add.group();
     platforms.enableBody = true;
-    const ground = platforms.create(0, this.game.world.height - 64, 'platform');
+    const ground = platforms.create(0, this.world.height - 64, 'platform');
     ground.scale.setTo(2, 2);
     ground.body.imovable = true;
     let ledge = platforms.create(100, 200, 'platform');
@@ -25,25 +25,51 @@ export default class extends Phaser.State {
     ledge.scale.setTo(0.5, 0.9);
     ledge.body.immovable = true;
 
+    //create mushroom
+    this.mushroom = new Mushroom({
+      game: this,
+      x: this.world.centerX,
+      y: this.world.centerY,
+      asset: 'mushroom',
+    });
+    this.add.existing(this.mushroom);
+    this.physics.arcade.enable(this.mushroom);
+    this.mushroom.body.bounce.y = 0.2;
+    this.mushroom.body.gravity.y = 300;
+    this.mushroom.body.collideWorldBounds = true;
+
+    this.shroom = new Mushroom({
+      game: this,
+      x: this.world.centerX,
+      y: 0,
+      asset: 'mushroom',
+    });
+    this.add.existing(this.shroom);
+    this.physics.arcade.enable(this.shroom);
+    this.shroom.body.bounce.y = 0.2;
+    this.shroom.body.gravity.y = 300;
+    this.shroom.body.collideWorldBounds = true;
+
     //create player
-    const player = this.game.add.sprite(
-      32,
+    this.player = this.game.add.sprite(
+      this.world.centerX,
       this.game.world.height - 150,
       'dude'
     );
-    this.game.physics.arcade.enable(player);
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    this.game.physics.arcade.enable(this.player);
+    this.player.body.bounce.y = 0.2;
+    this.player.body.gravity.y = 300;
+    this.player.body.collideWorldBounds = true;
+    this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+    this.player.animations.add('right', [5, 6, 7, 8], 10, true);
 
     const cursors = this.game.input.keyboard.createCursorKeys();
   }
 
   update() {
     //console.log(this.game.physics.arcade.collide);
-    //this.game.physics.arcade.collide(this.player, this.platforms);
+    this.game.physics.arcade.collide(this.mushroom, this.shroom);
+    this.game.physics.arcade.collide(this.player, this.mushroom);
   }
 
   render() {}
